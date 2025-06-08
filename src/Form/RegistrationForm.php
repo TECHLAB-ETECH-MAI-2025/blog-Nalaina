f8c66781f21fd09cca9fc4e53ad0695dfccf4108
Nalaina <nofonalaina@gmail.com>
2025-06-08 22:43:24 +0300

    create the sign in form

---
+++ src/Form/RegistrationForm.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Dom\Text;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', Text::class, [
+                'label' => 'Prénom',
+                'required' => false,
+                'attr' =>[
+                    'placeholder' => 'Votre prénom',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastName', Text::class, [
+                'label' => 'Nom',
+                'required' => false,
+                'attr' =>[
+                    'placeholder' => 'Votre nom',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'exemple@domaine.com',
+                    'class' => 'form-control'
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veillez entrer votre adresse email',
+                    ]),
+                    // new Email([
+                    //     'message' => 'L\'adresse email "{{ value }}" n\'est pas une adresse valide.',
+                    // ]),
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'label' => 'J\'accepte les conditions d\'utilisation',
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter nos conditions d\'utilisation.',
+                    ]),
+                ],
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label',
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Minimum 8 caractères',
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control',
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer le mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez votre mot de passe',
+                        'autocomplete' => 'new-password',
+                        'class' => 'form-control',
+                    ],
+                ],
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez entrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
