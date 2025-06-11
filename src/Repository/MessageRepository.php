bf5fd2b275fee4eadd6a16722559a3569201881f
Nalaina <nofonalaina@gmail.com>
2025-06-11 20:28:03 +0300

    update the messageRepository

--- src/Repository/MessageRepository.php
+++ src/Repository/MessageRepository.php
@@ -16,28 +16,18 @@ class MessageRepository extends ServiceEntityRepository
         parent::__construct($registry, Message::class);
     }
 
-    //    /**
-    //     * @return Message[] Returns an array of Message objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('m.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Message
-    //    {
-    //        return $this->createQueryBuilder('m')
-    //            ->andWhere('m.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+       /**
+        * @return Message[] Returns an array of Message objects
+        */
+       public function findConversation(int $userId1, int $userId2): array
+       {
+           return $this->createQueryBuilder('m')
+               ->where('(m.sender = :user1 AND m.receiver = :user2) OR (m.sender = :user2 AND m.receiver = :user1)')
+               ->setParameter('user1', $userId1)
+               ->setParameter('user2', $userId2)
+               ->orderBy('m.createdAt', 'ASC')
+               ->getQuery()
+               ->getResult()
+           ;
+       }
 }
