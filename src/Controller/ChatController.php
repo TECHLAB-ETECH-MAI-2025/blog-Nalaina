9f2b4ed0d7446035b848b4846cdc5ecf734708c6
Nalaina <nofonalaina@gmail.com>
2025-06-11 20:18:36 +0300

    edidt the ChatController content

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Entity\User;
+use App\Form\MessageForm;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 final class ChatController extends AbstractController
 {
     #[Route('/chat', name: 'app_chat')]
-    public function index(): Response
+    public function index(
+        int $receiverId,
+        MessageRepository $messageRepository,
+        EntityManagerInterface $entityManager,
+        Request $request
+    ): Response
     {
+        /** @var User $currentUser */
+        $currentUser = $this->getUser();
+        if (!$currentUser instanceof UserInterface){
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+
+        $receiver = $entityManager->getRepository(User::class)->find($receiverId);
+        if (!$receiver) {
+            throw $this->createNotFoundException('Utilisateur non trouvé.');
+        }
+
+        $messages = $messageRepository->findConversation($currentUser->getId(), $receiverId);
+
+        $message = new Message();
+        $form = $this->createForm(MessageForm::class, $message);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message->setSender($currentUser);
+            $message->setReceiver($receiver);
+            $message->setCreatedAt(new \DateTime());
+
+            $entityManager->persist($message);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_chat', ['receiverId' => $receiverId]);
+        }
         return $this->render('chat/index.html.twig', [
-            'controller_name' => 'ChatController',
+            'messages' => $messages,
+            'receiver' => $receiver,
+            'form' => $form->createView(),
         ]);
     }
 }
