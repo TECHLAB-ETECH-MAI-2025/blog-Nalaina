b094554b9211d6aaae8564de3c781d8caca45fce
Nalaina <nofonalaina@gmail.com>
2025-06-11 18:03:26 +0300

    update the entity Message

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -14,19 +14,19 @@ class Message
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[ORM\ManyToOne(inversedBy: 'sentMessages')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $sender = null;
 
-    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[ORM\ManyToOne(inversedBy: 'receivedMessages')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $receiver = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $content = null;
 
-    #[ORM\Column]
-    private ?\DateTimeImmutable $createdAt = null;
+    #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
+    private ?\DateTimeInterface $createdAt = null;
 
     public function getId(): ?int
     {
@@ -38,7 +38,7 @@ class Message
         return $this->sender;
     }
 
-    public function setSender(?User $sender): static
+    public function setSender(?User $sender): self
     {
         $this->sender = $sender;
 
@@ -50,7 +50,7 @@ class Message
         return $this->receiver;
     }
 
-    public function setReceiver(?User $receiver): static
+    public function setReceiver(?User $receiver): self
     {
         $this->receiver = $receiver;
 
@@ -62,19 +62,19 @@ class Message
         return $this->content;
     }
 
-    public function setContent(string $content): static
+    public function setContent(string $content): self
     {
         $this->content = $content;
 
         return $this;
     }
 
-    public function getCreatedAt(): ?\DateTimeImmutable
+    public function getCreatedAt(): ?\DateTimeInterface
     {
         return $this->createdAt;
     }
 
-    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
 
